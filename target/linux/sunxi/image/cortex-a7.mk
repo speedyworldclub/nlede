7ed26a19347cd94baaf2c219fe9a4d13a0e14a55
speedyworldclub <52553859+speedyworldclub@users.noreply.github.com>
2019-11-05 23:10:57 +0800

    Update cortex-a7.mk

--- target/linux/sunxi/image/cortex-a7.mk
+++ target/linux/sunxi/image/cortex-a7.mk
@@ -169,7 +169,7 @@ define Device/sun8i-h3-nanopi-duo2
 	r8169-firmware rpcd rpcd-mod-rrdns rrdtool1 rs9113-firmware rt2800-usb-firmware rt73-usb-firmware rtl8188eu-firmware rtl8192ce-firmware rtl8192cu-firmware rtl8192de-firmware \
 	rtl8192se-firmware rtl8192su-firmware rtl8821ae-firmware swconfigti-3410-firmware ti-5052-firmware triggerhappy uclibcxx uhttpd usb-modeswitch usbutils \
 	wireless-regdb wl12xx-firmware wl18xx-firmware  wpad-mini zlib \
-  	luci-app-travelmate travelmate \
+  	luci-app-travelmate travelmate 
   SUNXI_DTS:=sun8i-h3-nanopi-duo2
 endef
 
